34462b8895cc18c57b0b5559510efd508a64eeab
Md. Ariful Islam Saikat <saikat.arif85@gmail.com>
2019-08-31 12:04:03 +0600

    first commit

---
+++ app/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+	protected $guarded = [];
+
+    public function user(){
+
+    	return $this->belongsTo(User::class);
+    }
+
+    public function profileImage(){
+
+    	$imagePath = ($this->image) ? $this->image : 'profile/jPEDYRjCxN5e7162nt4RJrXGaO8lOFvfeKtlkaBs.jpeg';
+
+    	return  '/storage/' . $imagePath; 
+    }
+
+    public function followers(){
+
+    	return $this->belongsToMany(User::class);
+    }
+
+}
---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to GeekInsta
+
+This is a community of fellow developer and we love that you have joined us.
+
+
+
+All the best,<br>
+GeekInsta Team
+@endcomponent
